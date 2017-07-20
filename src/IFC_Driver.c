adfbadfa14bc1be16ff19d8be98cac1dbe83e835
k-shishkin <k-shishkin>
2017-07-20 15:22:21 +0300

    mongoose crash when i try use FreeRtos in own library

--- src/IFC_Driver.c
+++ src/IFC_Driver.c
@@ -1,11 +1,17 @@
 #include "IFC_Driver.h"
 
+
 // NOTE: library init function must be called mgos_LIBNAME_init()
 bool mgos_IFC_Driver_init(void) {
   return true;
 }
-void test (int led, int state){
-  mgos_gpio_set_mode(led,1/*MGOS_GPIO_MODE_OUTPUT*/);
-  mgos_gpio_write (led, state);
-  return;
+void led_start (void *pvParameters){
+    int *led_on;
+    led_on = (int * )pvParameters;
+    mgos_gpio_set_mode(*led_on, MGOS_GPIO_MODE_OUTPUT);
+    mgos_gpio_write (*led_on, 1);
+}
+
+void test_on (int led){
+  xTaskCreate(led_start, "led_on", 1024,(void *)led, 6, NULL);
 }
--- src/IFC_Driver.h
+++ src/IFC_Driver.h
@@ -1,7 +1,11 @@
 #include "fw/src/mgos_init.h"  
 #include <stdbool.h>
 #include "fw/src/mgos_gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+
 
 bool mgos_IFC_Driver_init(void);   // Required - initialisation function
 
-void test(int led, int state);
+void test_on (int led);
+void led_start (void *pvParameters);
